77203c447dacf3215decca0d08fd078636029846
Fabio Henrique Cardoso <slynrick@gmail.com>
2018-02-23 23:08:28 -0300

    Send and Receive func implemented

--- SciChain/SciChain/scichain.cs
+++ SciChain/SciChain/scichain.cs
@@ -19,9 +19,12 @@ using System.Linq;
          * Process_Rejected  1
          * Waiting_Editor_Acceptance  2
          * Waiting_article  3
-         * Waiting_approval,  4
-         * Waiting_for_publication  5
-         * Published 6
+         * Waiting_rating 4
+         * Waiting_approval,  5
+         * Waiting_decrypto_article 6
+         * waiting_reviewers_approval 7
+         * Waiting_for_publication  8
+         * Published 9
       
     1: key do autor/autores ( 32 bytes )
     2: key do editor ( 32 bytes )
@@ -49,8 +52,9 @@ namespace Neo.SmartContract
         private static byte[] reviewersPrefix = { 4 };
         private static byte[] processPrefix = { 5 };
         private static byte[] publishPrefix = { 6 };
+        private static byte[] reviewerCommentsPrefix = { 7 };
 
-        public static object Main( string operation, params object[] args )
+        /*public static object Main( string operation, params object[] args )
         {
 
             if( operation == "GetProcessStatus()" )
@@ -80,7 +84,7 @@ namespace Neo.SmartContract
             if( operation == "Publish()" )
             {
                 if( args.Length != 2 ) return false;
-                return Publish( (byte[])args[0], (byte[])args[1] );
+                return Publish( (byte[])args[0] );
             }
 
             if( operation == "RegisterEditor()" )
@@ -96,6 +100,11 @@ namespace Neo.SmartContract
             }
 
             return false;
+        }
+        */
+
+        public static void Main(string operation, params object[] args)
+        {
         }
 
         public static byte GetProcessStatus( byte[] processkey )
@@ -149,33 +158,257 @@ namespace Neo.SmartContract
 
         public static bool SendDataToProcess( byte[] processkey, byte[] data )
         {
+            if ( data.Length == 0 )
+            {
+                Runtime.Notify( "Empty data" );
+                return false;
+            }
+
             byte[] ownAdress = ExecutionEngine.CallingScriptHash;
-            // dependendo da parte do processo, essa função vai interpretar de uma forma
-            // Os revisores e o escritor podem enviar dados pra o editor
-            // apenas o editor pode enviar dados para os revisores
-            // apenas o editor pode enviar dados para o escritor
-            return true;
+            byte[] processData = Storage.Get( Storage.CurrentContext, processkey );
+            byte status = processData[0];
+
+            if( status == 0 )
+            {
+                Runtime.Notify( "Can't send data to a undocumented process key" );
+                return false;
+            }
+
+            if( status == 1 )
+            {
+                Runtime.Notify( "Can't send data to a rejected process key" );
+                return false;
+            }
+
+            if( status == 8 )
+            {
+                Runtime.Notify( "Can't send data to a process key that's waiting to be published" );
+                return false;
+            }
+
+            if ( status == 9 )
+            {
+                Runtime.Notify( "Can't send data to a published process key" );
+                return false;
+            }
+
+            if ( status == 2 )
+            {
+                byte[] editorKey = editorPrefix;
+                editorKey.Concat( ownAdress );
+                editorKey = Hash256( editorKey );
+
+                if( processData.Range( 33, 32 ) == editorKey )
+                {
+                    Runtime.Notify( "Not the article editor" );
+                    return false;
+                }
+
+                if ( data[0] != 1 || data[0] != 3 )
+                {
+                    Runtime.Notify( "A status data must be Rejected(1) or Waiting article(3)" );
+                    return false;
+                }
+
+                processData = processData.Range( 0, 65 );
+
+                
+                processData[0] = data[0];
+                processData.Concat( data.Range( 1, data.Length - 1 ) ); // colocando os dados dos revisores ( número de revisores ( 1 byte ) + conjunto de 32 bytes a key de cada editor
+                Storage.Put( Storage.CurrentContext, processkey, processData );
+
+                return true;
+            }
+
+            if( status == 3 )
+            {
+                byte[] autorKey = autorPrefix;
+                autorKey.Concat( ownAdress );
+                autorKey = Hash256( autorKey );
+
+                if( processData.Range( 1, 32 ) == autorKey )
+                {
+                    Runtime.Notify( "Not the article autor" );
+                    return false;
+                }
+
+                processData[0] = 4;
+                processData.Concat( new byte[] { 0 } ); // um byte contador de avaliações
+                processData.Concat( data ); // enviando a chave simétrica criptografada com a chave publica de cada revisor + artigo criptografado com a chave simétrica
+                Storage.Put( Storage.CurrentContext, processkey, processData );
+                return true;
+            }
+
+            if( status == 4 )
+            {
+                byte[] reviewerKey = reviewerPrefix;
+                reviewerKey.Concat( ownAdress );
+                reviewerKey = Hash256( reviewerKey );
+
+                int idx = ( 66 + 32 * processData[65] );
+                for ( int i = 66; i < ( 66 + 32*processData[65] ); i += 32 )
+                {
+                    if( processData.Range( i, 32 ) == reviewerKey )
+                    {
+                        byte[] reviewerCommentsKey = reviewerCommentsPrefix;
+                        reviewerCommentsKey.Concat( processkey );
+                        reviewerCommentsKey.Concat( reviewerKey );
+                        reviewerCommentsKey = Hash256( reviewerCommentsKey );
+
+                        byte[] reviewerComments = Storage.Get( Storage.CurrentContext, reviewerCommentsKey );
+
+                        if( reviewerCommentsKey.Length > 0 )
+                        {
+                            Runtime.Notify( "Already rated" );
+                            return false;
+                        }
+
+                        Storage.Put( Storage.CurrentContext, reviewerCommentsKey, data );
+
+                        processData[idx]++;
+                        if (processData[idx] == processData[65])
+                            processData[0] = 5;
+
+                        processData.Concat( reviewerCommentsKey );
+                        Storage.Put( Storage.CurrentContext, processkey, processData );
+                        return true;
+                    }
+                }
+
+                Runtime.Notify( "Not the article reviewer" );
+                return false;
+            }
+
+            if( status == 5 )
+            {
+                byte[] editorKey = editorPrefix;
+                editorKey.Concat( ownAdress );
+                editorKey = Hash256( editorKey );
+
+                if( processData.Range( 33, 32 ) == editorKey )
+                {
+                    Runtime.Notify( "Not the article editor" );
+                    return false;
+                }
+
+                if ( data[0] != 1 || data[0] != 6 )
+                {
+                    Runtime.Notify( "A status data must be Rejected(1) or Waiting publishing(6)" );
+                    return false;
+                }
+
+                processData[0] = data[0];
+                processData = processData.Range( 0, ( 66 + 32 * processData[65] ) ); // limpando dados anteriores
+
+                byte[] numApproval = new byte[processData[65]];
+
+                for( int i = 0; i < numApproval.Length; i++ )
+                    numApproval[i] = 0;
+                
+                processData.Concat( numApproval ); // adicionando campos para os revisores avaliarem se o artigo que o autor colocará sem criptografia foi o mesmo que eles avaliaram.
+                                                   // 1 não aprovado e 2 aprovado
+                Storage.Put( Storage.CurrentContext, processkey, processData );
+                return true;
+            }
+
+            if( status == 6 )
+            {
+                byte[] autorKey = autorPrefix;
+                autorKey.Concat( ownAdress );
+                autorKey = Hash256( autorKey );
+
+                if( processData.Range( 1, 32 ) == autorKey )
+                {
+                    Runtime.Notify( "Not the article autor" );
+                    return false;
+                }
+
+                processData[0] = 7;
+                processData.Concat( data ); // artigo descriptografado
+                Storage.Put( Storage.CurrentContext, processkey, processData );
+                return true;
+            }
+
+            if ( status == 7 )
+            {
+                if( data[0] != 1 || data[0] != 2 )
+                {
+                    Runtime.Notify( "Data must be Rejected(1) or Aprroved(2)" );
+                    return false;
+                }
+
+                byte[] reviewerKey = reviewerPrefix;
+                reviewerKey.Concat( ownAdress );
+                reviewerKey = Hash256( reviewerKey );
+
+                int numA = 0;
+                for( int i = 66, count = 0; i < (66 + 32 * processData[65]); i += 32, count++ )
+                {
+                    int idx = ( 66 + 32 * processData[65] ) + count;
+                    if( processData[idx] != 0 )
+                        numA++;
+                    if ( processData.Range( i, 32 ) == reviewerKey )
+                    {
+                        processData[idx] = data[0];
+                    }
+                }
+
+                if( numA == processData[65] )// todos avaliaram
+                {
+                    processData[0] = 8;
+                    Storage.Put( Storage.CurrentContext, processkey, processData );
+                }
+                return true;
+            }
+
+            Runtime.Notify( "Not the article reviewer" );
+            return false;
         }
 
         public static byte[] ReceiveFromProcess( byte[] processkey )
         {
             byte[] ownAdress = ExecutionEngine.CallingScriptHash;
-            // dependendo da parte do processo, essa função vai interpretar de uma forma
-            // Os revisores e o escritor podem enviar dados pra o editor
-            // apenas o editor pode enviar dados para os revisores
-            // apenas o editor pode enviar dados para o escritor
-            return null;
-        }
+            byte[] processData = Storage.Get( Storage.CurrentContext, processkey );
 
-        public static bool Publish( byte[] data, byte[] processkey )
-        {
-            if( data.Length <= 0 )
+            byte[] autorKey = autorPrefix;
+            autorKey.Concat( ownAdress );
+            autorKey = Hash256( autorKey );
+
+            if( processData.Range( 1, 32 ) != autorKey )
             {
-                Runtime.Notify( "No data found" );
-                return false;
+                byte[] editorKey = editorPrefix;
+                editorKey.Concat( ownAdress );
+                editorKey = Hash256( editorKey );
+
+                if( processData.Range( 33, 32 ) != editorKey )
+                {
+                    byte[] reviewerKey = reviewerPrefix;
+                    reviewerKey.Concat( ownAdress );
+                    reviewerKey = Hash256( reviewerKey );
+
+                    bool ok = false;
+                    for( int i = 66; i < ( 66 + 32 * processData[65] ); i += 32 )
+                    {
+                        if ( processData.Range( i, 32 ) == reviewerKey )
+                        {
+                            ok = true;
+                        }
+                    }
+
+                    if( !ok )
+                    {
+                        Runtime.Notify( "Access denied" );
+                        return null;
+                    }
+                }   
             }
 
-            if( GetProcessStatus( processkey ) != 5 )
+            return processData;
+        }
+
+        public static bool Publish( byte[] processkey )
+        {
+            if( GetProcessStatus( processkey ) != 8 )
             {
                 Runtime.Notify( "Can't publish" );
                 return false;
@@ -199,9 +432,9 @@ namespace Neo.SmartContract
 
             byte[] processes = Storage.Get( Storage.CurrentContext, epKey );
 
-            for( int i = 0; i < processes.Length; i += 256 )
+            for( int i = 0; i < processes.Length; i += 32 )
             {
-                if( processes.Range( i, 256 ) == processkey )
+                if( processes.Range( i, 32 ) == processkey )
                 {
                     byte[] publishKey = publishPrefix;
                     publishKey.Concat( processkey );
@@ -213,9 +446,7 @@ namespace Neo.SmartContract
                         return false;
                     }
 
-                    data.Concat( processkey );
-
-                    Storage.Put( Storage.CurrentContext, publishKey, data );
+                    Runtime.Notify( "Published" );
                     return true;
                 }
             }
@@ -264,9 +495,9 @@ namespace Neo.SmartContract
 
             byte[] reviewers = Storage.Get( Storage.CurrentContext, reviewersKey );
 
-            for( int i = 0; i < reviewers.Length; i += 256 )
+            for( int i = 0; i < reviewers.Length; i += 32 )
             {
-                if( reviewers.Range( i, 256 ) == ReviewerAdress )
+                if( reviewers.Range( i, 32 ) == ReviewerAdress )
                 {
                     Runtime.Notify( "Reviwer already registered" );
                     return false;
