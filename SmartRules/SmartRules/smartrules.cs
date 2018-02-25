9794a349a958bc9d99d941dfd8a2ce9917f6c442
Fabio Henrique Cardoso <slynrick@gmail.com>
2018-02-25 11:19:39 -0300

    Adding SmartRules

---
+++ SmartRules/SmartRules/smartrules.cs
@@ -0,0 +1,15 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace Neo.SmartContract
+{
+    public class SmartRules : Framework.SmartContract
+    {
+        public static bool Main( byte[] processkey, string opt )
+        {
+            return true;
+        }
+    }
+}
